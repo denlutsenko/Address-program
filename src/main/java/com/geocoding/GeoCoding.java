98c883df6cd994a2d6f28607f11c10e790f664c0
denlutsenko <lutsenko.d@ipland.com.ua>
2017-05-04 17:16:03 +0300

    commit  google  geoCoding

---
+++ src/main/java/com/geocoding/GeoCoding.java
@@ -0,0 +1,50 @@
+package com.geocoding;
+
+import com.entity.StoreAddress;
+import com.google.common.collect.Maps;
+import org.apache.log4j.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * Created by lutsenko.d on 10.04.2017.
+ */
+public class GeoCoding {
+    private static final Logger LOG = Logger.getLogger(GeoCoding.class);
+    private JsonReader jsonReader;
+    private final String BASE_URL = "http://maps.googleapis.com/maps/api/geocode/json";
+
+
+    public GeoCoding() {
+        jsonReader = new JsonReader();
+    }
+
+    public List<StoreAddress> findCoordinates(List<StoreAddress> tmpAddresses) {
+
+        for (StoreAddress address : tmpAddresses) {
+            final Map<String, String> params = Maps.newHashMap();
+            params.put("sensor", "false");
+            params.put("address", address.getStoreAddress());
+            final String url = BASE_URL + '?' + jsonReader.encodeParams(params);
+            try {
+                JSONObject response = jsonReader.readJson(url);
+                JSONObject location = response.getJSONArray("results").getJSONObject(0);
+                location = location.getJSONObject("geometry");
+                location = location.getJSONObject("location");
+                double longitude = location.getDouble("lng");
+                double latitude = location.getDouble("lat");
+                address.setLatitude(latitude);
+                address.setLongitude(longitude);
+            } catch (JSONException | IOException e) {
+                LOG.error("can't find GPS coordinates, " + e + "\n"
+                         + "address TT: " + address.getStoreAddress() + "\n");
+            }
+        }
+        return tmpAddresses;
+    }
+}
---
+++ src/main/java/com/geocoding/JsonReader.java
@@ -0,0 +1,48 @@
+package com.geocoding;
+
+import com.google.common.base.Joiner;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Created by Den on 3/31/2017.
+ */
+public class JsonReader {
+
+    public JSONObject readJson(final String url) throws IOException, JSONException {
+        try (InputStream is = new URL(url).openStream()) {
+            final BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            final String jsonText = readAll(rd);
+            return new JSONObject(jsonText);
+        }
+    }
+
+    private  String readAll(final Reader rd) throws IOException {
+        final StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+
+    public  String encodeParams(final Map<String, String> params) {
+        return Joiner.on('&').join(params.entrySet().stream().map(input -> {
+            try {
+                return input.getKey() + '=' + URLEncoder.encode(input.getValue(), "utf-8");
+            } catch (final UnsupportedEncodingException e) {
+                throw new RuntimeException(e);
+            }
+        }).collect(Collectors.toList()));
+    }
+}
+
+
